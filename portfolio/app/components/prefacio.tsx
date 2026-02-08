7bcca8d32966b2c4bdbb1682cb805b15b1486628
vesquicio <victosousa92@gmail.com>
2026-02-08 10:59:05 -0300

    update: prefacio

--- portfolio/app/components/prefacio.tsx
+++ portfolio/app/components/prefacio.tsx
@@ -2,7 +2,7 @@
 import Image from "next/image";
 import { useEffect } from "react";
 import AOS from 'aos';
-import { Augusta } from "../fonts";
+import { Augusta, Creato } from "../fonts";
 
 export default function Prefacio() {
 
@@ -22,12 +22,16 @@ export default function Prefacio() {
 
         <div className="text-center md:text-right pl-3 ">
 
-          <h2 data-aos="fade-down" className={`text-3xl text-light ${Augusta.className}`}>Um pouco sobre mim...</h2>
+          <h2 data-aos="fade-down" className={`text-3xl mb-5 text-light ${Augusta.className}`}>Um pouco sobre mim...</h2>
 
-            <p data-aos="fade-up" className="">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus, necessitatibus. Molestias quibusdam ipsam impedit deleniti laboriosam voluptatibus dolorem at, veritatis sint ex animi, ipsa beatae similique nemo recusandae quisquam magnam! 
+            <p data-aos="fade-up" className={`mb-5 ${Creato.className}`}>
             
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime earum suscipit at, nostrum veritatis praesentium laborum ut, atque ipsam libero reprehenderit aspernatur? Officiis eum dolores fuga tempore molestiae, eos similique?
+            Como descrever-me, sem faltar aquilo que é essencial? Me chamo João Victor, e sempre fui uma pessoa que admirou a arte, a tecnologia e a capacidade de se criar de tudo. Desde muito novo me envolvia em ideias mirabolantes, como desenhar os menus dos DVDs no meu caderno sem nem saber escrever ou criar uma ficha técnica inteira de um filme, escolhendo até mesmo o elenco que estrelaria nele.
+            
             </p>
+
+            <p data-aos="fade-up" className={` ${Creato.className}`}>
+            Fui crescendo e evolui essas ideias, e mesmo com o mundo ficando mais sério, mantive aquele capricho infantil, decidindo continuar desenvolvendo ideias criativas, agora com as novas ferramentas que aprendi. Seja desenho digital, escrita ou, é claro, programação, gosto de estar a frente de projetos que me desafiem mas que ainda mantenham aquele brilho essencial. Sempre ao fazer um projeto, penso: <q>esse é o tipo de coisa que eu gostaria de ter, e que ainda não é tão explorado?</q>, o que me ajuda a navegar por caminhos menos ortodoxos e que ainda assim apresentem uma familiaridade confortante. </p> 
             
         
         </div>
